7fe2afb1366cd39c6df0dc0e38b316660406ad05
Cristian Ferrari <cristianferrarig@gmail.com>
2012-07-04 23:23:15 -0400

    first view almost ready

--- source/javascripts/plugins.js
+++ source/javascripts/plugins.js
@@ -1,1 +1,2 @@
-//= require "libs/_jquery-1.7.1"
+//= require "lib/jquery-1.7.2"
+//= require "lib/jquery.backbroundstretch"
--- source/javascripts/script.js
+++ source/javascripts/script.js
@@ -1,6 +1,85 @@
+var goToArticle = function(elements, callback, returnValue) {
+  if (!returnValue)
+      returnValue = false;
+
+  if ('ontouchstart' in document.documentElement) {
+    elements.unbind().bind('touchstart', function(event_start) {
+      $(this).addClass('active');
+      this.moved = false;
+      elements.unbind('touchmove').bind('touchmove', function(e) {
+        this.moved = true;
+      });
+      elements.unbind('touchend').bind('touchend', function(e) {
+        $(this).removeClass('active');
+        if( !this.moved ) {
+          if (typeof callback == 'function') {
+            $(this).removeClass('active');
+            var self = this;
+            setTimeout(function(){
+              callback.call(self, event_start);
+            }, 0);
+            return !returnValue;
+          }
+        }
+      });
+    });
+  } else {
+    elements.unbind().bind('click', function(e) {
+      if (typeof callback == 'function') {
+        var self = this;
+        setTimeout(function(){
+          callback.call(self, e);
+        }, 0);
+      }
+    })
+  }
+}
 
 $(document).ready(function(){
+
+  var main = $('#main');
+
+  goToArticle($('a[data-target]'), function(event) {
+    var windowHeight = $(window).height();
+    var index = $(this).attr('data-target') - 1;
+    var height = windowHeight;
+    var move = 'translate3d(0px, ' + (-index*height) + 'px, 0px)';
+    main.css({
+      '-moz-transform': move,
+      '-webkit-transform': move,
+      'transform': move
+    });
+  });
+
   $(window).resize(function(){
-    setBackgroundStretch('#wrapper');
+
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+
+    $('article').each(function(i,e) {
+      var target = $(e);
+      var inner = target.find('.inner');
+      var innerHeight = inner.height();
+      var innerMargin = (windowHeight - innerHeight) / 2;
+      target.css({
+        'width': windowWidth,
+        'height': windowHeight
+      });
+      inner.css({
+        'padding-top': innerMargin,
+        'padding-bottom': innerMargin
+      });
+    });
+
+    var articleCount = $('article').length();
+    var mainHeight = windowHeight * articleCount;
+
+    main.css({
+      'height': mainHeight
+    });
+
+    //setBackgroundStretch('section');
+
   }).resize();
+
 });
